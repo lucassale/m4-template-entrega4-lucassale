dd65c773335b695a4992cdb4b471449f40bc4105
Carlos Lucas Sales de Andrade <132520068+lucassale@users.noreply.github.com>
2024-09-03 20:27:17 -0300

    Update database.

--- sql/database.sql
+++ sql/database.sql
@@ -45,7 +45,7 @@ INSERT INTO authors (name, bio)
 VALUES 
     ('Eiichiro Oda', 'Eiichiro Oda em um mangaká conhecido pela criação do mangá One Piece.') 
 RETURNING id, name, bio;
-
+    
 INSERT INTO authors (name, bio) 
 VALUES 
     ('J. K. Rowling', 'J. K. Rowling é uma escritora, roteirista e produtora cinematográfica britânica, notória por escrever a série de livros Harry Potter.') 
